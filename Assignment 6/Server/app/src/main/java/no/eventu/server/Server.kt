745fe2315f94729fb2e2920ab8da6f3fdc2a91b7
Eventu <even.tuverud@gmail.com>
2022-10-13 20:05:16 +0200

    make server send message back to client

--- Assignment 6/Server/app/src/main/java/no/eventu/server/Server.kt
+++ Assignment 6/Server/app/src/main/java/no/eventu/server/Server.kt
@@ -14,7 +14,7 @@ import java.net.ServerSocket
 import java.net.Socket
 
 private const val PORT = 12345
-class Server(private val textView: TextView) {
+class Server(private val serverLog: TextView) {
 
     fun start(){
         CoroutineScope(Dispatchers.IO).launch {
@@ -29,7 +29,7 @@ class Server(private val textView: TextView) {
                         addToServerLog("En Klient koblet seg til: \n $clientSocket")
                         //ui = "En Klient koblet seg til: \n$clientSocket"
 
-                        sendToClient(clientSocket, "Velkommen klient!")
+                        sendToClient(clientSocket, "server", "Velkommen klient!")
 
                         readFromClient(clientSocket)
                     }
@@ -47,13 +47,16 @@ class Server(private val textView: TextView) {
 
     private fun readFromClient(socket: Socket) {
         val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+        val username = reader.readLine()
         val message = reader.readLine()
-        addToServerLog("From Client: $message")
+        addToServerLog("From Client: $username : $message")
+        sendToClient(socket, username, message)
         //ui = "Klienten sier:\n$message"
     }
 
-    private fun sendToClient(socket: Socket, message: String) {
+    private fun sendToClient(socket: Socket, username: String, message: String) {
         val writer = PrintWriter(socket.getOutputStream(), true)
+        writer.println(username)
         writer.println(message)
         addToServerLog("To Clients: $message")
 
@@ -62,15 +65,15 @@ class Server(private val textView: TextView) {
 
     private fun addToServerLog(string: String){
         MainScope().launch {
-            val oldString = textView.text.toString()
-            textView.text = oldString + "\n" + string + "\n"
+            val oldString = serverLog.text.toString()
+            serverLog.text = oldString + "\n" + string + "\n"
         }
     }
 
     private var ui: String? = ""
         set(str){
             MainScope().launch {
-                textView.text =  str + "\n"
+                serverLog.text =  str + "\n"
                 field = str
             }
         }
