1bd0329724a169de7937f488dfdc77678c6599e8
Eventu <even.tuverud@gmail.com>
2022-10-13 14:34:00 +0200

    Rename value val Log

--- Assignment 6/Server/app/src/main/java/no/eventu/server/MainActivity.kt
+++ Assignment 6/Server/app/src/main/java/no/eventu/server/MainActivity.kt
@@ -2,10 +2,25 @@ package no.eventu.server
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val log = findViewById<TextView>(R.id.messages)
+        Server(log).start()
     }
+
+    /**
+     *
+
+    fun addToServerLog(logstring: String){
+        val log = findViewById<TextView>(R.id.messages)
+        var oldLog = log.text
+        var newLog = oldLog.toString() + "\n" + logstring
+        log.setText(newLog)
+    }*/
 }
