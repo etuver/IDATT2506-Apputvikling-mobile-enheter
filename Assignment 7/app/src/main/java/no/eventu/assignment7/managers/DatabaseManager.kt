9f231401f9db6a044a1f63a94435b426e73bc387
Eventu <even.tuverud@gmail.com>
2022-11-07 20:03:10 +0100

    Reads from file ok

--- Assignment 7/app/src/main/java/no/eventu/assignment7/managers/DatabaseManager.kt
+++ Assignment 7/app/src/main/java/no/eventu/assignment7/managers/DatabaseManager.kt
@@ -1,4 +1,100 @@
 package no.eventu.assignment7.managers
 
-class DatabaseManager {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+open class DatabaseManager(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
+
+    companion object {
+
+        const val DATABASE_NAME = "movieDatabase"
+        const val DATABASE_VERSION = 1
+
+        const val ID = "_id"
+
+        //Director
+        const val TABLE_DIRECTOR = "DIRECTOR"
+        const val DIRECTOR_NAME = "name"
+        const val DIRECTOR_ID = "director_id"
+
+        //Movies
+        const val TABLE_MOVIE = "MOVIE"
+        const val MOVIE_TITLE = "title"
+        const val MOVIE_ID = "movie_id"
+
+        //Actors
+        const val TABLE_ACTOR = "ACTOR"
+        const val ACTOR_NAME = "name"
+        const val ACTOR_ID = "actor_id"
+
+
+        const val TABLE_DIRECTOR_MOVIE = "DIRECTOR_MOVIE"
+        const val TABLE_ACTOR_MOVE = "ACTOR_MOVIE"
+
+        val JOIN_DIRECTOR_MOVIE = arrayOf(
+            "$TABLE_DIRECTOR.$ID=$TABLE_DIRECTOR_MOVIE.$DIRECTOR_ID",
+            "$TABLE_MOVIE.$ID=$TABLE_DIRECTOR_MOVIE.$MOVIE_ID"
+        )
+
+        val JOIN_ACTOR_MOVIE = arrayOf(
+            "$TABLE_ACTOR.$ID=$TABLE_ACTOR_MOVE.$ACTOR_ID",
+            "$TABLE_MOVIE.$ID=$TABLE_ACTOR_MOVE.$MOVIE_ID"
+        )
+
+
+
+
+    }
+
+
+    /**
+     * Create the tables
+     */
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
+            """create table $TABLE_DIRECTOR(
+            $ID integer primary key autoincrement,
+            $DIRECTOR_NAME text unique not null
+            );
+            """.trimIndent()
+        )
+        db.execSQL(
+            """create table $TABLE_ACTOR(
+            $ID integer primary key autoincrement,
+            $ACTOR_NAME text unique not null
+            );""".trimIndent()
+        )
+        db.execSQL(
+            """create table $TABLE_MOVIE(
+            $ID integer primary key autoincrement,
+            $MOVIE_TITLE text unique not null
+            );""".trimIndent()
+        )
+        db.execSQL(
+            """create table $TABLE_DIRECTOR_MOVIE(
+            $ID integer primary key autoincrement,
+            $MOVIE_ID numeric,
+            $DIRECTOR_ID numeric,
+            FOREIGN KEY($DIRECTOR_ID) REFERENCES $TABLE_DIRECTOR($ID),
+            FOREIGN KEY($MOVIE_ID) REFERENCES $TABLE_MOVIE($ID)
+            );""".trimIndent()
+        )
+        db.execSQL(
+            """create table $TABLE_ACTOR_MOVE(
+            $ID integer primary key autoincrement,
+            $MOVIE_ID numeric,
+            $ACTOR_ID numeric,
+            FOREIGN KEY($ACTOR_ID) REFERENCES $TABLE_ACTOR($ID),
+            FOREIGN KEY($MOVIE_ID) REFERENCES $TABLE_MOVIE($ID)
+            );""".trimIndent()
+        )
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+
+
 }
--- Assignment 7/app/src/main/java/no/eventu/assignment7/managers/FileManager.kt
+++ Assignment 7/app/src/main/java/no/eventu/assignment7/managers/FileManager.kt
@@ -1,12 +1,72 @@
 package no.eventu.assignment7.managers
 
+import android.app.Activity
 import android.util.Log
 import no.eventu.assignment7.Movie
 import java.io.BufferedReader
+import java.io.File
 import java.io.FileReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 
-class FileManager {
+class FileManager(private val activity: Activity) {
+    private val filename: String = "newLocalFile.txt"
+    private val dir: File = activity.filesDir
+    private val file: File = File(dir, filename)
 
+    //private val FILEPATH = "/res/raw/movies.txt"
+    //private val file: File = File(FILEPATH)
+
+
+
+    fun readMoviesFromTxt(fileId: Int): ArrayList<Movie>{
+        val moviesList = ArrayList<Movie>()
+
+        try {
+            val inputStream: InputStream = activity.resources.openRawResource(fileId)
+            BufferedReader(InputStreamReader(inputStream)).use {
+                reader ->
+                //Log.e("yes", fileId.toString())
+                var title = reader.readLine()
+                var director = reader.readLine()
+                var actors = reader.readLine().split(',')
+                reader.readLine()
+                while (title != null){
+                    moviesList.add(Movie(title, director, actors))
+                    title = reader.readLine()
+                    if (title != null){
+                        director = reader.readLine()
+                        actors = reader.readLine().split(',')
+                    }
+                    reader.readLine()
+                }
+            }
+
+        } catch (e: IOException){
+            Log.e("Error reading file: ",e.toString())
+        }
+        return moviesList
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // ----------- OLD for csv ----------
 
     /**
      *
@@ -35,4 +95,3 @@ class FileManager {
 
     }
 */
-}
